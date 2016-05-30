9a5473acb3ccb1ee02c504f7e80bda5a9a614b7c
Your Name <you@example.com>
2016-05-30 18:49:43 +0900

    画像追加

--- app/src/main/java/com/example/kouhei/omikuzi_test_app/MainActivity.java
+++ app/src/main/java/com/example/kouhei/omikuzi_test_app/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.kouhei.omikuzi_test_app;
 
-import android.media.MediaPlayer;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     String s;
+    Uri uri;
+
+    InputStream istream[] = new InputStream[4];
 
 
     @Override
@@ -20,26 +31,61 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void getomikuji(View view) {
-    TextView tv = (TextView) findViewById(R.id.display);
-    Random randomGenerator = new Random();
-        int i = randomGenerator.nextInt(3);
+        TextView tv = (TextView) findViewById(R.id.display);
+        ImageView im = (ImageView) findViewById(R.id.picture);
+        Bitmap bitmap = null;
 
-        switch(i){
+        try {
+            istream[0] = getResources().getAssets().open("pic_01.jpg");
+            istream[1] = getResources().getAssets().open("pic_02.jpg");
+            istream[2] = getResources().getAssets().open("pic_03.jpg");
+            istream[3] = getResources().getAssets().open("pic_04.jpg");
+
+
+            // im.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            Log.d("Assets", "Error");
+        }
+
+
+        Random randomGenerator = new Random();
+        int i = randomGenerator.nextInt(4);
+
+        switch (i) {
             case 0:
-                s = ("吉");
+                s = ("大吉");
+                bitmap = BitmapFactory.decodeStream(istream[0]);
+                im.setImageBitmap(bitmap);
 
                 break;
             case 1:
-                s = ("凶");
+                s = ("小吉");
+                bitmap = BitmapFactory.decodeStream(istream[1]);
+                im.setImageBitmap(bitmap);
 
                 break;
             case 2:
-                s = ("大凶");
+                s = ("中吉");
+                bitmap = BitmapFactory.decodeStream(istream[2]);
+                im.setImageBitmap(bitmap);
 
                 break;
+            case 3:
+                s = ("吉");
+                bitmap = BitmapFactory.decodeStream(istream[3]);
+                im.setImageBitmap(bitmap);
+
+                break;
+
         }
 
-        tv.setText(s);
+       // tv.setText(s);
+        //im.setImageBitmap(bitmap);
+
+//        Uri.parse("https://twitter.com/intent/tweet?text="+"占った結果、今日の運勢は"+s);
+//
+//        Intent z = new Intent(Intent.ACTION_VIEW,uri);
+//        startActivity(z);
 
     }
 }
